9c59ba8c6007d73012569b096a13838a1dde1f62
TheChozoLord <91266947+TheChozoLord@users.noreply.github.com>
2024-01-26 09:54:10 -0500

    Add files via upload

--- MagicEightBallDay1.py
+++ MagicEightBallDay1.py
@@ -16,19 +16,24 @@ fortune = [
 
 print("Hello! What do you want to do?"
       " 1: show all fortunes"
-      " 2: get your fortune told"
-      " 3: pick specific fortune"
+      " 2: pick specific fortune"
+      " 3: get your fortune told"
       " 4: leave")
 
-menuInput = (input("enter number of option you wish to persue"))
+menuInput = (input("enter number of option you wish to persue: "))
 
-if menuInput == "2":
+if menuInput == "3":
     fortuneRandomizer = random.randint(0, 9)
+    input("What is your question? ")
     print (fortune[fortuneRandomizer])
 if menuInput == "1":
-    print(fortune) 
-if menuInput == "3":
+    for i, fortune in enumerate(fortune):
+        print(f"{i} (fortune[i])") 
+if menuInput == "2":
     specific = input("Please input the number of the fortune you want:")
-    print (fortune[specific])
-else:
+    specificInt = int(specific)
+    print (fortune[specificInt])
+if menuInput =="4":
     print ("Probably for the best.")
+else:
+    print("Please enter a valid input next time")
